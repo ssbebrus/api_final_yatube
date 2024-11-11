74d287adcea9db70d52faf39f24a37cb18fd46b4
Сёма Селиванов <bebrus@MacBook-Air-Sema.local>
2024-11-11 18:05:33 +0500

    fixed flake8 issues

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -11,4 +11,5 @@ class OwnerOrReadOnly(permissions.BasePermission):
             )
 
     def has_object_permission(self, request, view, obj):
-        return obj.owner == request.user or request.method in permissions.SAFE_METHODS
+        return (obj.owner == request.user or
+                request.method in permissions.SAFE_METHODS)
